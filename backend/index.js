f73e8769e3a8adf7a21eb72f02adad0b5891f250
harrykwan <33515754+harrykwan@users.noreply.github.com>
2020-06-04 12:29:07 +0800

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static('html/public'))
+app.get('/', (req, res) => res.sendfile('html/landing.html'))
+app.get('/chooseside', (req, res) => res.sendfile('html/chooseside.html'))
+// app.get('/login', (req, res) => res.sendfile('html/login.html'))
+app.get('/logintutor', (req, res) => res.sendfile('html/tutorlogin.html'))
+app.get('/loginstudent', (req, res) => res.sendfile('html/studentlogin.html'))
+// app.get('/register', (req, res) => res.sendfile('html/register.html'))
+app.get('/registertutor', (req, res) => res.sendfile('html/tutorregister.html'))
+app.get('/registerstudent', (req, res) => res.sendfile('html/studentregister.html'))
+app.get('/profile', (req, res) => res.sendfile('html/profile.html'))
+app.get('/home', (req, res) => res.sendfile('html/index.html'))
+
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
