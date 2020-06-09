1848f3faac7ddeee208d64e0810cd3d46dca636b
System Administrator <root@Harrys-MacBook-Pro.local>
2020-06-09 15:34:53 +0800

    fixed profile page config problem

--- backend/index.js
+++ backend/index.js
@@ -2,18 +2,43 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.static('html/public', { root: __dirname }))
-app.get('/', (req, res) => res.sendFile('html/landing.html', { root: __dirname }))
-app.get('/chooseside', (req, res) => res.sendFile('html/chooseside.html', { root: __dirname }))
+app.use(express.static('html/public', {
+    root: __dirname
+}))
+app.get('/', (req, res) => res.sendFile('html/landing.html', {
+    root: __dirname
+}))
+app.get('/chooseside', (req, res) => res.sendFile('html/chooseside.html', {
+    root: __dirname
+}))
 // app.get('/login', (req, res) => res.sendFile('html/login.html', { root: __dirname }))
-app.get('/logintutor', (req, res) => res.sendFile('html/tutorlogin.html', { root: __dirname }))
-app.get('/loginstudent', (req, res) => res.sendFile('html/studentlogin.html', { root: __dirname }))
+app.get('/logintutor', (req, res) => res.sendFile('html/tutorlogin.html', {
+    root: __dirname
+}))
+app.get('/loginstudent', (req, res) => res.sendFile('html/studentlogin.html', {
+    root: __dirname
+}))
 // app.get('/register', (req, res) => res.sendFile('html/register.html', { root: __dirname }))
-app.get('/registertutor', (req, res) => res.sendFile('html/tutorregister.html', { root: __dirname }))
-app.get('/registerstudent', (req, res) => res.sendFile('html/studentregister.html', { root: __dirname }))
-app.get('/profile', (req, res) => res.sendFile('html/profile.html', { root: __dirname }))
-app.get('/home', (req, res) => res.sendFile('html/index.html', { root: __dirname }))
-app.get('/emailconfirm', (req, res) => res.sendFile('html/emailconfirm.html', { root: __dirname }))
+app.get('/registertutor', (req, res) => res.sendFile('html/tutorregister.html', {
+    root: __dirname
+}))
+app.get('/registerstudent', (req, res) => res.sendFile('html/studentregister.html', {
+    root: __dirname
+}))
+// app.get('/profile', (req, res) => res.sendFile('html/profile.html', { root: __dirname }))
+app.get('/profilestudent', (req, res) => res.sendFile('html/studentprofile.html', {
+    root: __dirname
+}))
+app.get('/profiletutor', (req, res) => res.sendFile('html/tutorprofile.html', {
+    root: __dirname
+}))
+
+app.get('/home', (req, res) => res.sendFile('html/index.html', {
+    root: __dirname
+}))
+app.get('/emailconfirm', (req, res) => res.sendFile('html/emailconfirm.html', {
+    root: __dirname
+}))
 
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
