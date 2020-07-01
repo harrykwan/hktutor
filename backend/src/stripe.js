2db223596d36e796480f948ef00a66cc54d95986
harrykwan <harry1998kwan@gmail.com>
2020-07-01 22:23:16 +0800

    stripe test

--- backend/src/stripe.js
+++ backend/src/stripe.js
@@ -4,15 +4,16 @@ const stripe = require('stripe')('sk_test_CRl2Sto6lG7ZEjnvDIfJxtZx00uRtS3tJx');
 
 async function getclientsecret(callback) {
     const paymentIntent = await stripe.paymentIntents.create({
-        amount: 1,
+        amount: 400,
         currency: 'hkd',
         // Verify your integration in this guide by including this parameter
         metadata: {
             integration_check: 'accept_a_payment'
         },
     });
-    callback(paymentIntent.intent.client_secret)
+    console.log(paymentIntent)
+    callback(paymentIntent.client_secret)
 }
 
 
-exports.getclientsecret = this.getclientsecret
+exports.getclientsecret = getclientsecret
