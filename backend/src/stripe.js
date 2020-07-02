e516a42ad94d5a7e7f667e99a2f0d128823330dd
Jack Chan <46509894+jackchan0528@users.noreply.github.com>
2020-07-02 23:47:54 +0800

    alert

--- backend/src/stripe.js
+++ backend/src/stripe.js
@@ -1,4 +1,5 @@
-const stripe = require('stripe')('sk_test_CRl2Sto6lG7ZEjnvDIfJxtZx00uRtS3tJx');
+// const stripe = require('stripe')('sk_test_CRl2Sto6lG7ZEjnvDIfJxtZx00uRtS3tJx');
+const stripe = require('stripe')('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');
 
 
 
