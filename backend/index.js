a08c99cbbdacab13875d26cfc0cb8fef6ecc50f7
Harry Kwan <harry1998kwan@gmail.com>
2020-07-28 20:16:56 +0800

    home page

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.static('html/public', {
     root: __dirname
 }))
-app.get('/', (req, res) => res.sendFile('html/landing.html', {
+app.get('/', (req, res) => res.sendFile('html/home.html', {
     root: __dirname
 }))
 app.get('/chooseside', (req, res) => res.sendFile('html/chooseside.html', {
